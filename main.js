e46d7bc52e0ca19217ed092ffab0ac9dbc14c573
Shayan-Bravo <shayan.khosravi@ontariotechu.net>
2021-12-28 16:25:50 -0500

    updated

--- main.js
+++ main.js
@@ -70,9 +70,9 @@ window.onload = function (){
     ];
 
     // max characters on canvas
-    let maxCharCount = 30;
+    let maxCharCount = 50;
     let fallingCharArr = [];
-    let fontSize = 10;
+    let fontSize = 13;
     // number of columns on screen
     let maxColumns = canvasWidth / fontSize;
     canvas.width = canvasWidth;
@@ -84,6 +84,8 @@ window.onload = function (){
         constructor(x, y) {
             this.x = x;
             this.y = y;
+            this.colourList = ["rgba(0,255,0)", "rgba(255,0,0)", "rgba(255,255,255)"];
+            this.randomColour = Math.floor(Math.random() * 3);
         }
 
         draw(ctx) {
@@ -91,12 +93,10 @@ window.onload = function (){
             this.value =
             charArr[Math.floor(Math.random() * (charArr.length - 1))].toUpperCase();
             //speed is a random value
-            this.speed = (Math.random() * fontSize * 2) / 4 + (fontSize * 2) / 4;
+            this.speed = (Math.random() * fontSize * 4) / 10 + (fontSize * 4) / 10;
 
             // generate random index between 0 and 2
-            let colourList = ["rgba(0,255,0)", "rgba(255,0,0)", "rgba(255,255,255)"];
-            let randomIndex = Math.floor(Math.random() * 3);
-            ctx.fillStyle = "rgba(0,255,0)";
+            ctx.fillStyle = this.colourList[this.randomColour];
             ctx.font = fontSize + "px san-serif";
 
             ctx.fillText(this.value, this.x, this.y);
@@ -105,7 +105,7 @@ window.onload = function (){
             if (this.y > canvasHeight) {
                 this.y = (Math.random() * canvasHeight) / 2 - 50;
                 this.x = Math.floor(Math.random() * maxColumns) * fontSize;
-                this.speed = (-Math.random() * fontSize * 2) / 4 + (fontSize * 2) / 4;
+                this.speed = (-Math.random() * fontSize * 4) / 10 + (fontSize * 4) / 10;
             }
         }
     }
